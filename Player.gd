6ad908211e68517ba99c10781a077bb14951408c
Igor Borges <igor@borges.dev>
2024-08-08 15:59:35 -0300

    Export variables

--- Player.gd
+++ Player.gd
@@ -1,17 +1,19 @@
 class_name Player extends RigidBody3D
 
 
-var boost_force: float = 1000.0
-var torque_force: float = 100.0
+## Rocket thrust (how much vertical force is applied when moving).
+@export_range(800.0, 3000.0) var thrust: float = 1500.0
+## Rocket torque (how much force is applied to "turn" the rocket).
+@export_range(100.0, 300.0) var torque: float = 200.0
 
 
 func _process(delta: float) -> void:
     if Input.is_action_pressed("boost"):
-        apply_central_force(basis.y * boost_force * delta)
+        apply_central_force(basis.y * thrust * delta)
     if Input.is_action_pressed("rotate_cw"):
-        apply_torque(Vector3(0, 0, torque_force * delta))
+        apply_torque(Vector3(0, 0, torque * delta))
     if Input.is_action_pressed("rotate_ccw"):
-        apply_torque(Vector3(0, 0, -torque_force * delta))
+        apply_torque(Vector3(0, 0, -torque * delta))
 
 
 func _on_body_entered(body: Node) -> void:
